eb3833d4241b18d606c0e721cf9fdfe858ce7f3f
aa00 <aa00@hp.com>
2020-11-30 15:34:46 +0800

    change js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,8 @@ $(window).resize(sectionHeight);
 
 $(function() {
   $("section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.]/g,'-') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.]/g,'-'));
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/]/g,'-') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/]/g,'-'));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
@@ -26,7 +26,7 @@ $(function() {
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();
-    console.log($(this).text)  
+    //console.log($(this).text)  
   });
 
   sectionHeight();
@@ -36,7 +36,7 @@ $(function() {
 
 $(document).ready(function() {
     $("aside ul li a").on("click", function (e) {
-       console.log('a click: scrollTop=' +$("aside").scrollTop())
+       //console.log('a click: scrollTop=' +$("aside").scrollTop())
        document.cookie = "scrollTop=" + $("aside").scrollTop() + "; path=/"; 
        return true;
     });    
@@ -44,7 +44,7 @@ $(document).ready(function() {
         var arr = document.cookie.match(/scrollTop=([^;]+)(;|$)/); 
         scroll_top = parseInt(arr[1])
         $("aside").animate({scrollTop: scroll_top}, 0);
-        console.log('ready: scrollTop=' + scroll_top )
+        //console.log('ready: scrollTop=' + scroll_top )
     }   
 })
 
@@ -52,6 +52,6 @@ window.onload = function() {
     if(document.cookie.match(/scrollTop=([^;]+)(;|$)/) != null) {
         var arr = document.cookie.match(/scrollTop=([^;]+)(;|$)/); 
         scroll_top = parseInt(arr[1])
-        console.log('onload: scrollTop=' + scroll_top )
+        //console.log('onload: scrollTop=' + scroll_top )
     }       
 }
