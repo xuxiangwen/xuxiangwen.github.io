38c0a5c80301aefc34ce7392146512ce8396255f
aa00 <aa00@hp.com>
2020-11-30 16:08:28 +0800

    change js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,8 @@ $(window).resize(sectionHeight);
 
 $(function() {
   $("section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/]/g,'-') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/]/g,'-'));
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/\$]/g,'-') + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[\+\=\(\),:：\.\{\}\/\$]/g,'-'));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
