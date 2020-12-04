7bd35bf6d9db3192e172785060d8d852b18b2dbf
aa00 <aa00@hp.com>
2020-12-04 21:07:47 +0800

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,21 +12,41 @@ var sectionHeight = function() {
 
 var scroll_to_anchor = function(pos, scroll_time){
   var position = pos - 90; 
-//   console.log('scroll_to_position:' + position)
   $("html, body").animate({scrollTop: position}, scroll_time);
 }    
+
+var binary_search = function(x, item) {
+    low = 0
+    high = x.length-1
+    while (low<high){
+        mid = parseInt((low + high)/2)
+        if (x[mid]==item) {
+            high = mid        
+        } else if (x[mid]>item) {
+            high = mid-1                             
+        } else {
+            low = mid+1
+        }
+    }  
+    if (x[low]>item) return low -1
+    return low
+}
 
 var  locate_catalog = function(){
   var headers = $('section h2, section h3');
   var catalog = $('nav ul li a');    
   var scroll_height = $(window).scrollTop()+120;
-  for(var i =0;i<headers.length;i++){
-    var header_height = $(headers[i]).offset().top;    
-    if (header_height<scroll_height){  
-      catalog.removeClass('active'); 
-      $(catalog[i]).addClass('active');
-    }
+  var positions = [];
+  console.log(positions)
+  for(let i =0;i<headers.length;i++){
+    positions[i] =  $(headers[i]).offset().top
   }
+  var index = binary_search(positions, scroll_height) 
+  catalog.removeClass('active'); 
+  console.log(index, scroll_height)
+  if (index >= 0) {
+    $(catalog[index]).addClass('active');      
+  }    
 }
 
 $(window).resize(sectionHeight);
@@ -50,11 +70,7 @@ $(function() {
   });
 
   $("nav ul li").on("click", "a", function(event) {     
-    scroll_to_anchor($($(this).attr("href")).offset().top, 200)  
-//     setTimeout(() => {
-//       $("nav ul li a").removeClass('active'); 
-//       $(this).addClass('active');
-//     }, 410);        
+    scroll_to_anchor($($(this).attr("href")).offset().top, 200)        
     event.preventDefault();
   });
 
