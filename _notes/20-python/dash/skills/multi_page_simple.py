2453768c2ef9049efb99e875d05c42f4a5ad2b3b
aa00 <aa00@hp.com>
2021-01-13 19:59:56 +0800

    d

---
+++ _notes/20-python/dash/skills/multi_page_simple.py
@@ -0,0 +1,35 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+print(dcc.__version__) # 0.6.0 or above is required
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
+
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-2"', href='/page-2'),
+
+    # content will be rendered in this element
+    html.Div(id='page-content')
+])
+
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    return html.Div([
+        html.H3('You are on page {}'.format(pathname))
+    ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
